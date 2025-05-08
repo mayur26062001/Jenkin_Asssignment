629bc550033e579d3cbeca0cea522067e31f44ee
mayur26062001 <mayurj@pre-scient.com>
2025-05-08 10:28:15 +0530

    Update mail.bat

--- script/mail.bat
+++ script/mail.bat
@@ -11,16 +11,6 @@ set "SMTP_USER=mayurjungari1@gmail.com"  REM Gmail address for authentication
 set "SMTP_PASS=%SMTP_PASS%"  REM This is securely injected in Jenkins
 
 REM Send email using PowerShell
-powershell -ExecutionPolicy Bypass -Command ^
-  "$smtpServer = 'smtp.gmail.com';" ^
-  "$smtpFrom = 'mayurjungari1@gmail.com';"  REM The sender's email address (your Gmail address) ^
-  "$smtpTo = '%TO_EMAIL%';" ^
-  "$smtpUser = '%SMTP_USER%';" ^
-  "$smtpPass = '%SMTP_PASS%';" ^
-  "$msg = New-Object System.Net.Mail.MailMessage($smtpFrom, $smtpTo, '%SUBJECT%', '%BODY%');" ^
-  "$smtp = New-Object Net.Mail.SmtpClient($smtpServer, 587);" ^
-  "$smtp.EnableSsl = $true;" ^
-  "$smtp.Credentials = New-Object System.Net.NetworkCredential($smtpUser, $smtpPass);" ^
-  "$smtp.Send($msg);"
+powershell -ExecutionPolicy Bypass -Command "$smtpServer = 'smtp.gmail.com'; $smtpFrom = 'mayurjungari1@gmail.com'; $smtpTo = '%TO_EMAIL%'; $smtpUser = '%SMTP_USER%'; $smtpPass = '%SMTP_PASS%'; $msg = New-Object System.Net.Mail.MailMessage($smtpFrom, $smtpTo, '%SUBJECT%', '%BODY%'); $smtp = New-Object Net.Mail.SmtpClient($smtpServer, 587); $smtp.EnableSsl = $true; $smtp.Credentials = New-Object System.Net.NetworkCredential($smtpUser, $smtpPass); $smtp.Send($msg);"
 
 endlocal
